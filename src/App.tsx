deb621c4e24bab66bb7cf259ae116e814e53a3bc
Patrick <patrickpqdt87289@gmail.com>
2022-05-13 11:59:31 -0300

    interface-leftside

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import * as C from './styles/AppStyle';
 
-function App() {
+import logo from './images/powered.png';
+
+const App: React.FC = () => {
+
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  function handleCalculateImc() {
+    if (heightField || weightField) {
+        if (!heightField) {
+          alert('Digite a sua altura!');
+         
+          
+        } else if (!weightField) {
+          alert('Digite o seu peso!');
+          
+        }
+    } 
+    else {
+      alert('Insira ao menos uma altura e peso.')
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+        <C.Header>
+          <C.HeaderContainer>
+            <img src={logo} alt="logo" width={150}/>
+          </C.HeaderContainer>
+        </C.Header>
+        <C.Content>
+            <C.ContentLeftSide>
+              <h1>Calcule o seu IMC.</h1>
+              <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela (OMS) Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.</p>
+
+              <input 
+              type="number"
+              placeholder="Digite a sua Altura. Ex 1.5 (em metros)"
+              value={heightField > 0 ? heightField : ''}
+              onChange={e => setHeightField(parseFloat(e.target.value))}
+              />
+
+              <input 
+                type="number"
+                placeholder="Digite o seu peso. Ex 65.3 (Em KG)"
+                value={weightField > 0 ? weightField : ''}
+                onChange={e => setWeightField(parseFloat(e.target.value))}
+              />
+
+              <button onClick={handleCalculateImc}>Calcular IMC</button>
+            </C.ContentLeftSide>
+            <C.ContentRightSide>RIGHT</C.ContentRightSide>
+        </C.Content>
+    </C.Container>
   );
 }
 
---
+++ src/styles/AppStyle.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+export const Header = styled.header`
+  
+`;
+
+export const HeaderContainer = styled.div`
+    max-width: 900px;
+    margin: 40px auto;
+`;
+
+
+export const Content = styled.div`
+    display: flex;
+    max-width: 900px;
+    margin: auto;
+`;
+export const ContentLeftSide = styled.div`
+    flex: 1;
+    margin-right: 40px;
+
+    h1 {
+        margin: 0;
+        font-size: 40px;
+        color: #3A4B5C;
+    }
+
+    p {
+        font-size: 1rem;
+        margin-bottom: 40px;
+        color: #6A7D8B;
+    }
+
+    input {
+        width: 100%;
+        border: 0;
+        outline: none;
+        border-bottom: 2px solid rgba(150, 163, 171, .5);
+
+        padding: 10px 2px;
+        margin-bottom: 20px;
+        font-size: 14px;
+    }
+
+    button {
+        background: #227C9D;
+        color: #fff;
+        font-size: 15px;
+        border: 0;
+        border-radius: 10px;
+        padding: 15px 0;
+        width: 100%;
+
+        cursor: pointer;
+        margin-top: 40px;
+
+        transition: all .5s ease;
+        &:hover {
+            opacity: .8;
+        }
+    }
+`;
+
+
+
+
+export const ContentRightSide = styled.div`
+    flex: 1;
+    margin-left: 40px;
+`;
